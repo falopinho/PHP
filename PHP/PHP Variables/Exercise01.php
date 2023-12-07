a8a36697863e14a7bdd2b71a0df3b6c89350fbcb
Fabinho <116457022+falopinho@users.noreply.github.com>
2023-12-07 10:19:26 -0300

    Add files via upload

---
+++ PHP/PHP Variables/Exercise01.php
@@ -0,0 +1,6 @@
+<?php
+//Create a variable named txt and assign the value "Hello".
+
+$txt = "Hello"
+
+?>
---
+++ PHP/PHP Variables/Exercise02.php
@@ -0,0 +1,8 @@
+<?php
+//Create one variable named x, and one variable named y, then use the echo statement to output the sum of x and y.
+
+$x = 5;
+$y = 7;
+echo $x + $y;
+
+?>
